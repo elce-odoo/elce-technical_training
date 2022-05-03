2e0f00e68ad9dbedb97c2679d43e9bef0ea1b9d0
elce-odoo <elce@odoo.com>
2022-05-03 16:51:50 -0500

    Fields

--- odoo_moon/models/spaceship.py
+++ odoo_moon/models/spaceship.py
@@ -7,4 +7,15 @@ class Spaceship(models.Model):
     
     _name = "moon.spaceship"
     _description = "Spaceship info"
+
+    name = fields.Cahr(string='Title', required=True)
+    description = fields.Text(string='Description')
+
+    level = fields.Selection(string='Level'
+                            selection=[('beginner', 'Beginner'),
+                                        ('intermediate', 'Intermediate'),
+                                        ('advanced', 'Advanced')],
+                            copy=False)
+    
+    active = fields.Boolean(string='Active', default=True)
     
