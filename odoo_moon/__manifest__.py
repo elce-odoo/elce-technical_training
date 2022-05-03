f5ec411752222d0743ea7297e50a5f2e04ace56a
elce-odoo <elce@odoo.com>
2022-05-03 19:29:06 +0000

    [ADD] Moon Module

---
+++ odoo_moon/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo moon',
+    
+    'summary': """Moon app""",
+    
+    'description': """
+        Moon app to go to the moon:
+        [bla]
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ]
+}
