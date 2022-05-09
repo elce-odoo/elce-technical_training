34ebd723ed306f934ba87edcc63804ec8b543ec4
elce-odoo <elce@odoo.com>
2022-05-09 13:36:51 -0500

    yei

--- scripts/project_script.py
+++ scripts/project_script.py
@@ -7,3 +7,23 @@ password = 'admin'
 
 common = client.ServerProxy("{}/xmlrpc/2/common".format(url))
 print(common.version)
+
+uid = common.authenticate(db, username, password, {})
+print(uid)
+
+models = client.ServerProxy("{}/xmlrpc/2/object".format(url))
+
+model_access = models.execute_kw(db, uid, password,
+                                'project.project', 'check_access_rights',
+                                ['write'], {'raise_exception': False})
+print(model_access)    
+
+draft_quotes = models.execute_kw(db, uid, password,
+                                 'project.project', 'search',
+                                 [[['state', '=', 'draft']]])
+print(draft_quotes)
+
+if_confirmed = models.execute_kw(db, uid, password,
+                                 'project.project', 'action_confirm',
+                                 [draft_quotes])
+print(draft_quotes)
