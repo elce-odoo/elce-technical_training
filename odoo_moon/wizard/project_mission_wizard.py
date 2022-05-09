2ba2062305e74f535b4ec39a27f864c9233571f9
elce-odoo <elce@odoo.com>
2022-05-09 11:08:49 -0500

    asd

--- odoo_moon/wizard/project_mission_wizard.py
+++ odoo_moon/wizard/project_mission_wizard.py
@@ -2,28 +2,29 @@
 
 from odoo import models, fields, api
 
+
 class ProjectWizard(models.TransientModel):
     _name = 'moon.project.wizard'
     _description = 'Wizard: Quick '
 
     def _default_mission(self):
         return self.env['moon.mission'].browse(self._context.get('active_id'))
-    
+
     mission_id = fields.Many2one(comodel_name='moon.mission',
                                  string='Mission',
                                  required=True,
                                  default=_default_mission)
 
-
     mission_crew_ids = fields.Many2many(comodel_name='res.partner',
-                                 string='Crew in mission',
-                                 related='mission_id.crew_ids')
+                                        string='Crew in mission',
+                                        related='mission_id.crew_ids')
 
     crew_ids = fields.Many2many(comodel_name='res.partner',
-                         string='Crew for the project')
+                                string='Crew for the project')
 
     def create_projects(self):
-        mission_id = self.env['moon.mission'].search([('create_project', '=', True)], limit=1)
+        mission_id = self.env['moon.mission'].search(
+            [('create_project', '=', True)], limit=1)
         if mission_id:
             for member in self.crew_ids:
                 project_id = self.env['project.project'].create({
