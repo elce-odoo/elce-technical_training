996ef85086be84b36a5080de0ad2e2d4c5665920
elce-odoo <elce@odoo.com>
2022-05-09 13:49:42 -0500

    aver

--- scripts/spacehip_script.py
+++ scripts/spacehip_script.py
@@ -13,30 +13,30 @@ print(uid)
 
 models = client.ServerProxy("{}/xmlrpc/2/object".format(url))
 
-model_access = models.execute_kw(db, uid, password,
-                                'moon.spaceship', 'check_access_rights',
-                                ['write'], {'raise_exception': False})
-print(model_access)  
+# model_access = models.execute_kw(db, uid, password,
+#                                 'moon.spaceship', 'check_access_rights',
+#                                 ['write'], {'raise_exception': False})
+# print(model_access)  
 
-spaceships = models.execute(db, uid, password,
-                            'moon.spaceship', 'create',
-                            [
-                                {
-                                    'name': 'Apolo 20 mil',
-                                    'state': 'draft',
-                                    'active': True,
-                                    'height': 5,
-                                    'width': 2,
-                                    'depth': 3
-                                },
-                                {
-                                    'name': 'Apolo 21 mil',
-                                    'state': 'draft',
-                                    'active': True,
-                                    'height': 5,
-                                    'width': 2,
-                                    'depth': 3
-                                },
-                            ]
-                            )
-print(spaceships)
+# spaceships = models.execute(db, uid, password,
+#                             'moon.spaceship', 'create',
+#                             [
+#                                 {
+#                                     'name': 'Apolo 20 mil',
+#                                     'state': 'draft',
+#                                     'active': True,
+#                                     'height': 5,
+#                                     'width': 2,
+#                                     'depth': 3
+#                                 },
+#                                 {
+#                                     'name': 'Apolo 21 mil',
+#                                     'state': 'draft',
+#                                     'active': True,
+#                                     'height': 5,
+#                                     'width': 2,
+#                                     'depth': 3
+#                                 },
+#                             ]
+#                             )
+# print(spaceships)
