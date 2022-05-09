293012a5018c925f45191f66e594fa239690bc6c
elce-odoo <elce@odoo.com>
2022-05-09 15:58:04 -0500

    gjrj

--- scripts/spacehip_script.py
+++ scripts/spacehip_script.py
@@ -1,7 +1,7 @@
 from xmlrpc import client
 
-url = 'https://elce-odoo-elce-technical-training-moon-4862646.dev.odoo.com'
-db = 'elce-odoo-elce-technical-training-moon-4862646'
+url = 'https://elce-odoo-elce-technical-training-moon-4863071.dev.odoo.com'
+db = 'elce-odoo-elce-technical-training-moon-4863071'
 username = 'admin'
 password = 'admin'
 
@@ -14,30 +14,22 @@ print(uid)
 models = client.ServerProxy("{}/xmlrpc/2/object".format(url))
 
 
-# model_access = models.execute_kw(db, uid, password,
-#                                 'moon.spaceship', 'check_access_rights',
-#                                 ['write'], {'raise_exception': False})
-# print(model_access)  
+model_access = models.execute_kw(db, uid, password,
+                                'moon.spaceship', 'check_access_rights',
+                                ['write'], {'raise_exception': False})
+print(model_access)  
 
-# spaceships = models.execute(db, uid, password,
-#                             'moon.spaceship', 'create',
-#                             [
-#                                 {
-#                                     'name': 'Apolo 20 mil',
-#                                     'state': 'draft',
-#                                     'active': True,
-#                                     'height': 5,
-#                                     'width': 2,
-#                                     'depth': 3
-#                                 },
-#                                 {
-#                                     'name': 'Apolo 21 mil',
-#                                     'state': 'draft',
-#                                     'active': True,
-#                                     'height': 5,
-#                                     'width': 2,
-#                                     'depth': 3
-#                                 },
-#                             ]
-#                             )
-# print(spaceships)
+spaceships = models.execute(db, uid, password,
+                            'moon.spaceship', 'create',
+                            [
+                                {
+                                    'name': 'Apolo 20 mil',
+                                    'state': 'draft',
+                                    'active': True,
+                                    'height': 5,
+                                    'width': 2,
+                                    'depth': 3
+                                }
+                            ]
+                            )
+print(spaceships)
